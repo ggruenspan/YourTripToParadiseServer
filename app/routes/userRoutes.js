53b87a80c19a961fd5334a8aae8ab41b4054a731
Gerred Gruenspan <ggruenspan@outlook.com>
2023-08-23 23:28:56 -0400

    Added route for session

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,7 +1,20 @@
-module.exports = app => {
-    const user = require("../controllers/userController.js");
+const user = require("../controllers/userController.js");
+// const clientSessions = require("client-sessions");
 
+module.exports = app => {
     var router = require("express").Router();
+
+    // app.use(clientSessions( {
+    //     cookieName: "session",
+    //     secret: process.env.MONGODB_CONN_STR || "your-trip-to-paradise",
+    //     duration: 2*60*1000,
+    //     activeDuration: 1000*60
+    // }));
+    
+    // app.use(function(req, res, next) {
+    //     res.locals.session = req.session;
+    //     next();
+    // });
 
     // Registers a new user
     router.post("/register", function(req,res) {
@@ -14,21 +27,24 @@ module.exports = app => {
     router.post("/signIn", function(req,res) {
         req.body.userAgent = req.get('User-Agent');
         user.signIn(req.body)
-        .then(() => res.status(200).json({successMessage: "User signed in"}))
+        .then(user => {
+            req.session.user = {
+                userName: user.userName,
+                email: user.accountSetting.personalInfo.email
+            }
+            res.status(200).json({successMessage: "User signed in"});
+        })
         .catch(err => res.status(404).json({errorMessage: err}))
     });
 
-    // router.post("/signin", function(req,res) {
-    //     req.body.userAgent = req.get('User-Agent');
-    //     user.signIn(req.body)
-    //     .then(user => {
-    //         req.session.user = {
-    //             userName: user.userName,
-    //             email: user.email
-    //         }
-    //     })
-    //     .catch(err => res.status(404).json({errorMessage: err}))
-    // });
-
+    // User Session
+    router.get("/userData", function(req, res) {
+        const userData = {
+            userName: req.session.user.userName || 'Guest',
+            email: req.session.user.email || 'N/A',
+        };
+        res.json(userData);
+    });
+    
     app.use("/api", router);
 };
--- server.js
+++ server.js
@@ -6,14 +6,18 @@ var express = require("express");
 const userService = require("./app/controllers/userController.js");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const clientSessions = require("client-sessions");
 
 require('dotenv').config();
 const connectionString = process.env.MONGODB_CONN_STR;
 
-var corsOptions = { origin: "http://localhost:8081" };
-
 var app = express();
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: 'http://localhost:8081',
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,6 +42,27 @@ mongoose.connection.on("open", () => {
 //---------------------------------------------------------------------------
 /// END PRE LOADING DATABASE
 //---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+/// CREATES CLIENTSESSION
+//---------------------------------------------------------------------------
+
+app.use(clientSessions( {
+    cookieName: "session",
+    secret: process.env.MONGODB_CONN_STR || "your-trip-to-paradise",
+    duration: 2*60*1000,
+    activeDuration: 1000*60
+}));
+
+// app.use(function(req, res, next) {
+//     res.locals.session = req.session;
+//     next();
+// });
+
+//---------------------------------------------------------------------------
+/// END CREATES CLIENTSESSION
+//---------------------------------------------------------------------------
+
 
 //---------------------------------------------------------------------------
 /// ADDING ROUTES
